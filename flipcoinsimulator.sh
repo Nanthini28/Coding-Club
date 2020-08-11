ddbcf08bc1cad0cbd479f51fec6102f94d4fc8ad
Nanthini28 <68042825+Nanthini28@users.noreply.github.com>
2020-08-11 22:02:57 +0530

    Stage 2 - Day 10 Problems

---
+++ flipcoinsimulator.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+read -p "Enter the limit simulation:" value
+Head=1
+Tail=0
+declare -A Toss
+function check(){
+HCount=$1
+TCount=$2
+if (($HCount -le $TCount))
+then
+        echo "Tail won:" $((TCount-HCount))
+elif (($TCount -le $HCount))
+then
+        echo "Head won:" $((HCount-TCount))
+else
+        echo "TIE"
+fi
+}
+
+function Winner(){
+count=0
+HCount=0
+TCount=0
+
+while(($HCount<22 && $TCount<22 &&$count<$value))
+do
+ 	result=$((RANDOM%2))
+	if [[ $result -eq $Head ]]
+	then
+		Toss[H]=$((HCount++))
+	else
+		Toss[T]=$((TCount++))
+	fi
+	((count++))
+done
+check $HCount $TCount
+}
+
+function Tie(){
+while (($HCount-$TCount<2 && $TCount-$HCount<2))
+do
+        result=$((RANDOM%2))
+        if [[ $result -eq $Head ]]
+        then
+                Toss[H]=$((HCount++))
+        else
+                Toss[T]=$((TCount++))
+        fi
+        ((count++))
+done
+check $HCount $TCount
